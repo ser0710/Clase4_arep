a4164b5591ebd74e97b5d5dd0c72e9ae0a56e9f7
unknown <sergio.rozo-p@labinfo.is.escuelaing.edu.co>
2023-02-17 13:43:41 -0500

    primer commit

---
+++ src/main/java/eci/escuelaing/arep/app/Test.java
@@ -0,0 +1,8 @@
+package eci.escuelaing.arep.app;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Test {
+}
